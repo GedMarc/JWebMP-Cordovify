bada995ed23b493def05fb9a64b9ebee21f4a288
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:49:35 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,9 @@
+module com.jwebmp.plugins.cordovify {
+	exports com.jwebmp.plugins.cordovify;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+}
